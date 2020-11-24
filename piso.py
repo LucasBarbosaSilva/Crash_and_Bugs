16aff6f1733bc67c923ea8bd0a812d3915351d72
LucasBarbosaSilva <blucas.oficial@gmail.com>
2020-11-24 11:41:47 -0300

    piso question completed

--- piso.py
+++ piso.py
@@ -2,5 +2,8 @@ largura = int(input())
 comprimento = int(input())
 
 lajota_tipo1 = (largura * comprimento) + ((largura - 1) * (comprimento - 1))
+lajota_tipo2 = 2*((largura - 1) + (comprimento -1))
 
 print(lajota_tipo1)
+print(lajota_tipo2)
+
